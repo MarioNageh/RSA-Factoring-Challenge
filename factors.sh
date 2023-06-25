4c8f62364c48431594956365316314b6423864ee
Mario Nageh <marionageh7@gmail.com>
2023-06-25 13:09:06 +0300

    factors.sh

---
+++ factors.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def get_factors(n):
+    i = 2
+    for _ in range(n):
+        if n % i == 0:
+            return f"{n}={n//i}*{i}"
+        i += 1
+
+
+def main():
+    # Your Python script code here
+    # sys.argv[1] = "env"
+    # arg = sys.argv[1]
+    arg = "tests/test00"
+    print("The argument passed is:", arg)
+    filename = arg  # Replace with your file name
+
+    with open(filename, "r") as file:
+        for line in file:
+            i = int(line)
+            print(get_factors(i))
+
+
+if __name__ == "__main__":
+    main()
